3c3b4cbd9558f122642d537f31f738378b4aed5c
Victoria Kang <vickang@umich.edu>
2024-04-02 02:53:22 -0400

    new TextBuffer (works with public tests)

--- TextBuffer.cpp
+++ TextBuffer.cpp
@@ -1,192 +1,238 @@
-#include <stdio.h>
-#include <iostream>
-#include <string>
 #include "TextBuffer.hpp"
 
-using namespace std;
+  //EFFECTS: Creates a new editor with an empty text buffer, with the
+  //         current position at row 1 and column 0.
+  TextBuffer::TextBuffer(): row(1), column(0), index(0) {
+    cursor = data.begin();
+  }
 
-#include "TextBuffer.hpp"
-
-// Constructor
-TextBuffer::TextBuffer() {
-    cursor = data.end();
-    row = 1;
-    column = 0;
-    index = 0;
-}
-
-// Moves the cursor one position forward
-bool TextBuffer::forward() {
-    if (cursor == data.end()) {
-        return false;
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor one position forward and updates the
+  //          row and column, unless the cursor is already at the end
+  //          of the buffer, in which case this does nothing. Returns
+  //          true if the position changed, or false if it did not
+  //          (i.e. if the cursor was already at the end of the
+  //          buffer).
+  bool TextBuffer::forward() {
+    if(!is_at_end()){
+        if(*cursor == '\n'){
+            row++;
+            column = 0;
+        }
+        else{
+            column++;
+        }
+        cursor++;
+        index++;
+        return true;
     }
-    if (data_at_cursor() == '\n') {
-        row++;
-        column = 0;
-    } else {
-        column++;
+    return false;
+  }
+
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor one position backward and updates the
+  //          row and column, unless the cursor is already at the
+  //          start of the buffer, in which case this does nothing.
+  //          Returns true if the position changed, or false if it did
+  //          not (i.e. if the cursor was already at the start of the
+  //          buffer).
+  //HINT: Implement and use the private compute_column() member
+  //      function to compute the column when moving left from the
+  //      beginning of a line to the end of the previous one.
+  bool TextBuffer::backward() {
+    if (cursor != data.begin()) {
+        if (column == 0) {
+            row--;
+            cursor--;
+            index--;
+            column = compute_column();
+        } else {
+            cursor--;
+            index--;
+            column--;
+        }
+        return true;
     }
+    return false;
+  }
+
+  //
+
+  //MODIFIES: *this
+  //EFFECTS:  Inserts a character in the buffer at the cursor and
+  //          updates the current row and column.
+  void TextBuffer::insert(char c) { 
+  cursor = data.insert(cursor, c);
     index++;
-    cursor++;
-    return true;
-}
-
-// Moves the cursor one position backward
-bool TextBuffer::backward() {
-    if (cursor == data.begin()) {
-        return false;
-    }
-    cursor--;
-    index--;
-    if (*cursor == '\n') {
-        row--;
-        column = compute_column();
-    } else {
-        column--;
-    }
-    return true;
-}
-
-// Inserts a character at the cursor
-void TextBuffer::insert(char c) {
-    data.insert(cursor, c);
     if (c == '\n') {
-        row++;
-        column = 0;
+      row++;
+      cursor++;
+      index++;
+      column = 0;
     } else {
         column++;
+        cursor++;
+        index++;
     }
-    index++;
-}
+  }
 
-// Removes the character at the cursor
-bool TextBuffer::remove() {
-    if (cursor == data.end()) {
-        return false;
+  //MODIFIES: *this
+  //EFFECTS:  Deletes the character from the buffer that is at cursor.
+  //          Does nothing if the cursor is at the end of the buffer.
+  //          Returns true if a character was removed, or false if not
+  //          (i.e. if the cursor was at the end of the buffer).
+  bool TextBuffer::remove() {
+    if (!is_at_end()) {
+        cursor = data.erase(cursor);
+        return true;
     }
-    cursor = data.erase(cursor);
-    // Update row, column, and index as needed
-    // ...
-    return true;
-}
+    return false;
+  }
 
-// Moves the cursor to the start of the current row
-void TextBuffer::move_to_row_start() {
-    while (column != 0){
-      cursor--;
-      column--;
-      index--;
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor to the start of the current row (column
+  //          0).
+  void TextBuffer::move_to_row_start() {
+    while (column != 0){ 
+        backward(); 
     }
-}
+  }
 
-// Moves the cursor to the end of the current row
-void TextBuffer::move_to_row_end() {
-    while (cursor != data.end() && data_at_cursor()!='\n'){
-      ++cursor;
-      ++column;
-      ++index;
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor to the end of the current row (the
+  //          newline character that ends the row, or the end of the
+  //          buffer if the row is the last one in the buffer).
+  void TextBuffer::move_to_row_end() {
+    while (cursor != data.end() && *cursor != '\n') {
+        ++cursor;
+        ++index;
+        ++column;
     }
-}
+  }
 
-// Moves the cursor to the specified column in the current row
-void TextBuffer::move_to_column(int new_column) {
-    move_to_row_start();
-    while (column<=new_column && cursor != data.end() && data_at_cursor() != '\n'){
-      forward();
-    }
-}
 
-// Moves the cursor up to the previous row
-bool TextBuffer::up() {
-    int goalCol = column;
-    if(row == 1){
-      return false;
-    }
-    else{
-      move_to_row_start();
-      backward();
-      if (column >= goalCol){
-        move_to_row_end();
-      }
-      else{
-        while (column > goalCol){
-          backward();
+  //REQUIRES: new_column >= 0
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor to the given column in the current row,
+  //          or to the end of the row if the row does not have that
+  //          many columns.
+
+  void TextBuffer::move_to_column(int new_column) {
+    if (new_column >= 0) {
+        while (column > new_column && cursor != data.begin()) {
+            --cursor;
+            --index;
+            --column;
         }
-      }
-      return true;
+        while (column < new_column && cursor != data.end() && *cursor != '\n') {
+            ++cursor;
+            ++index;
+            ++column;
+        }
     }
-    return true;  // Or false, depending on the implementation
-}
+  }
 
-// Moves the cursor down to the next row
-bool TextBuffer::down() {
-    int colGoal = column;
-    move_to_row_end();
-    if (cursor == data.end()){
-      return false;
-    }
-    else{
-      forward();
-      move_to_row_end();
-      if (column > colGoal){
-        //cursor doesn't move
-      }
-      else{
+
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor to the previous row, retaining the
+  //          current column if possible. If the previous row is
+  //          shorter than the current column, moves to the end of the
+  //          previous row (the newline character that ends the row).
+  //          Does nothing if the cursor is already in the first row.
+  //          Returns true if the position changed, or false if it did
+  //          not (i.e. if the cursor was already in the first row).
+  bool TextBuffer::up() {
+    if (row > 1) {
         move_to_row_start();
-        while (column < colGoal){
-          forward();
+        if (backward()) {
+            move_to_row_end();
+            return true;
         }
-      
-      }
-      return true;
     }
-    return true;  // Or false, depending on the implementation
-}
+    return false;
+  }
 
-// Checks if the cursor is at the end of the text
-bool TextBuffer::is_at_end() const {
-    return cursor == data.end();
-}
+  //MODIFIES: *this
+  //EFFECTS:  Moves the cursor to the next row, retaining the current
+  //          column if possible. If the next row is shorter than the
+  //          current column, moves to the end of the next row (the
+  //          newline character that ends the row, or the end of the
+  //          buffer if it is the last row). Does nothing if the
+  //          cursor is already in the last row.
+  //          Returns true if the position changed, or false if it did
+  //          not (i.e. if the cursor was already in the last row).
+  bool TextBuffer::down() {
+    int original = column;
+    move_to_row_end();
+    if(cursor != data.end()){ 
+        forward();
+        move_to_column(original);
+    }
+    return false;
+  }
 
-// Returns the character at the cursor
-char TextBuffer::data_at_cursor() const {
+  //EFFECTS:  Returns whether the cursor is at the end of the buffer.
+  bool TextBuffer::is_at_end() const {
+    if(cursor == data.end()){
+        return true;
+    }
+    return false;
+  }
+
+  //REQUIRES: the cursor is not at the end of the buffer
+  //EFFECTS:  Returns the character at the current cursor
+  char TextBuffer::data_at_cursor() const {
     if (cursor != data.end()) {
         return *cursor;
     }
-    return '\0';  // Or another suitable default
-}
+    return '\0';
+  }
 
-// Returns the current row
-int TextBuffer::get_row() const {
+  int TextBuffer::get_row() const {
     return row;
-}
+  }
 
-// Returns the current column
-int TextBuffer::get_column() const {
+  //EFFECTS:  Returns the column of the character at the current
+  //          cursor.
+  int TextBuffer::get_column() const {
     return column;
-}
+  }
 
-// Returns the index of the current cursor position
-int TextBuffer::get_index() const {
-    return index;
-}
-
-// Returns the size of the text buffer
-int TextBuffer::size() const {
-    return static_cast<int>(data.size());
-}
-
-// Returns the contents of the text buffer as a string
-std::string TextBuffer::stringify() const {
-    return std::string(data.begin(), data.end());
-}
-
-// Computes the column of the cursor within the current row
-int TextBuffer::compute_column() const {
-    int column = 0;
-    std::list<char>::iterator it = cursor;
-    while (it != data.begin() && *(--it) != '\n') {
-        ++column;
+  //EFFECTS:  Returns the index of the character at the current cursor
+  //          with respect to the entire contents. If the cursor is at
+  //          the end of the buffer, returns size() as the index.
+  //HINT: Traversing a list is really slow. Instead, use the index
+  //      private member variable to keep track of the index and
+  //      update it as necessary.
+  int TextBuffer::get_index() const {
+    if(cursor == data.end()){ 
+        return data.size(); 
     }
-    return column;
-}
+    return index;
+  }
+
+  //EFFECTS:  Returns the number of characters in the buffer.
+  int TextBuffer::size() const {
+    return data.size();
+  }
+
+  //EFFECTS:  Returns the contents of the text buffer as a string.
+  //HINT: Implement this using the string constructor that takes a
+  //      begin and end iterator:
+  //        return std::string(data.begin(), data.end());
+  std::string TextBuffer::stringify() const {
+    return std::string(data.begin(), data.end());
+  }
+
+  //EFFECTS: Computes the column of the cursor within the current
+  //         row.
+  //NOTE: This does not assume that the "column" member variable has
+  //      a correct value (i.e. the second INVARIANT can be broken).
+  int TextBuffer::compute_column() const {
+     int col = 0;
+    Iterator it = cursor;
+    while (it != data.begin() && *(--it) != '\n') {
+        ++col;
+    }
+    return col;
+  }
